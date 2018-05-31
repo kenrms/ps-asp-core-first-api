473010f284f803ecb2ae10a7bdf2017ffe22f773
Kenneth Ramos <kramos@eduware.com>
2018-05-31 12:43:46 -0400

    Module 3 complete

---
+++ CityInfo.API/Controllers/CitiesController.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.API.Controllers
+{
+    [Route("api/cities")]
+    public class CitiesController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetCities()
+        {
+            return Ok(CitiesDataStore.Current.Cities);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCity(int id)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == id);
+
+            if (city == null) return NotFound();
+
+            return Ok(city);
+        }
+    }
+}
